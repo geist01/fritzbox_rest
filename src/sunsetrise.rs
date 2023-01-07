4c3d63212a60de4beac4f5ece888abf3a28395ad
Ingo Buescher <gallatin@gmx.net>
2023-01-08 00:16:13 +0100

    thiserror

--- src/sunsetrise.rs
+++ src/sunsetrise.rs
@@ -1,10 +1,10 @@
 use crate::konstanten::*;
-use crate::errors::*;
+use anyhow::Result;
 
 use chrono::prelude::*;
 
-pub fn fetch_sunset_intern(config : &Config) -> Result<SunsetRise> {
-    let now : DateTime<Utc> = Utc::now();
+pub fn fetch_sunset_intern(config: &Config) -> Result<SunsetRise> {
+    let now: DateTime<Utc> = Utc::now();
     let (sunrise, sunset) = sunrise::sunrise_sunset(
         config.lat.unwrap(),
         config.lng.unwrap(),
@@ -14,12 +14,13 @@ pub fn fetch_sunset_intern(config : &Config) -> Result<SunsetRise> {
     );
 
     // Construct a datetime from epoch:
-    let sunset_nd : DateTime<Utc> = Utc.from_utc_datetime(&NaiveDateTime::from_timestamp_opt(sunset, 0).unwrap());
-    let sunrise_nd : DateTime<Utc> = Utc.from_utc_datetime(&NaiveDateTime::from_timestamp_opt(sunrise, 0).unwrap());
+    let sunset_nd: DateTime<Utc> =
+        Utc.from_utc_datetime(&NaiveDateTime::from_timestamp_opt(sunset, 0).unwrap());
+    let sunrise_nd: DateTime<Utc> =
+        Utc.from_utc_datetime(&NaiveDateTime::from_timestamp_opt(sunrise, 0).unwrap());
 
     Ok(SunsetRise {
         sunset: sunset_nd,
         sunrise: sunrise_nd,
-
     })
 }
--- src/timertasks.rs
+++ src/timertasks.rs
@@ -1,19 +1,18 @@
 use chrono::prelude::*;
 use chrono::Duration;
 
-use crate::errors::*;
-use timer::Timer;
+use anyhow::Result;
+use std::sync::{Arc, Mutex};
 use std::thread;
-use std::sync::{Mutex, Arc};
+use timer::Timer;
 
-use crate::sunsetrise;
 use crate::konstanten::*;
 use crate::mobileping;
+use crate::sunsetrise;
 
 use log::*;
 
-
-pub fn init_timer(config : &Config) -> Result<()> {
+pub fn init_timer(config: &Config) -> Result<()> {
     // main timer once per day, starts ticker
 
     let config_clone = (*config).clone();
@@ -23,57 +22,62 @@ pub fn init_timer(config : &Config) -> Result<()> {
         let timer_clone = Arc::clone(&timer);
 
         let now: DateTime<Utc> = Utc::now() + Duration::seconds(2);
-        timer.lock().unwrap().schedule(now, Some(Duration::days(1)), move || {
-            debug!(target: "fritz", "Tages - Timer gestartet");
+        timer
+            .lock()
+            .unwrap()
+            .schedule(now, Some(Duration::days(1)), move || {
+                debug!(target: "fritz", "Tages - Timer gestartet");
 
-            if let Ok(sunsetrise) = sunsetrise::fetch_sunset_intern(&config_clone) {
-                debug!(target: "fritz", "{:?}", sunsetrise);
+                if let Ok(sunsetrise) = sunsetrise::fetch_sunset_intern(&config_clone) {
+                    debug!(target: "fritz", "{:?}", sunsetrise);
 
-                let config_clone2 = config_clone.clone();
+                    let config_clone2 = config_clone.clone();
 
-                let timer = timer_clone.lock().unwrap();
-                timer.schedule_with_date(sunsetrise.sunset - Duration::minutes(60), move || {
-                    debug!(target: "fritz", "Handler gestartet");
-                    let mut aktiv = if let Ok(()) = mobileping::ping_mobile(
-                        config_clone2.host_mobile.as_ref().unwrap(),
-                        config_clone2.port_mobile.as_ref().unwrap()) {
-                        debug!(target: "fritz", "Initialer Online Status: on");
-                        true
-                            
-                    } else {
-                        debug!(target: "fritz", "Initialer Online Status: off");
-                        false
-                    };
-                        
-                    loop {
-                        if let Ok(()) = mobileping::ping_mobile(
-                            config_clone2.host_mobile.as_ref().unwrap(),
-                            config_clone2.port_mobile.as_ref().unwrap()) {
-                            
-                            if !aktiv {
-                                debug!(target: "fritz", "online");
-                                aktiv = true;
+                    let timer = timer_clone.lock().unwrap();
+                    timer
+                        .schedule_with_date(sunsetrise.sunset - Duration::minutes(60), move || {
+                            debug!(target: "fritz", "Handler gestartet");
+                            let mut aktiv = if let Ok(()) = mobileping::ping_mobile(
+                                config_clone2.host_mobile.as_ref().unwrap(),
+                                config_clone2.port_mobile.as_ref().unwrap(),
+                            ) {
+                                debug!(target: "fritz", "Initialer Online Status: on");
+                                true
+                            } else {
+                                debug!(target: "fritz", "Initialer Online Status: off");
+                                false
+                            };
+
+                            loop {
+                                if let Ok(()) = mobileping::ping_mobile(
+                                    config_clone2.host_mobile.as_ref().unwrap(),
+                                    config_clone2.port_mobile.as_ref().unwrap(),
+                                ) {
+                                    if !aktiv {
+                                        debug!(target: "fritz", "online");
+                                        aktiv = true;
+                                    }
+                                } else if aktiv {
+                                    debug!(target: "fritz", "offline");
+                                    aktiv = false;
+                                }
+
+                                let time = Utc::now().time();
+                                if time.hour() >= 23 {
+                                    break;
+                                }
+
+                                thread::sleep(std::time::Duration::from_millis(10 * 1000));
                             }
-                            
-                        } else if aktiv {
-                            debug!(target: "fritz", "offline");
-                            aktiv = false;
-                        }
-                        
-                        let time = Utc::now().time();
-                        if time.hour() >= 23 {
-                            break;
-                        }
 
-                        thread::sleep(std::time::Duration::from_millis(10 * 1000));
-                    }
+                            debug!(target: "fritz", "Handler beendet");
+                        })
+                        .ignore();
+                }
+                debug!(target: "fritz", "Tages - Timer beendet");
+            })
+            .ignore();
 
-                    debug!(target: "fritz", "Handler beendet");
-                }).ignore();
-            }
-            debug!(target: "fritz", "Tages - Timer beendet");
-        }).ignore();
-        
         loop {
             thread::sleep(std::time::Duration::from_millis(1000));
         }
@@ -81,25 +85,20 @@ pub fn init_timer(config : &Config) -> Result<()> {
 
     let _res = child.join();
 
-// use tokio::prelude::*;
-// use tokio::timer::Interval;
+    // use tokio::prelude::*;
+    // use tokio::timer::Interval;
 
-// use std::time::{Duration, Instant};
+    // use std::time::{Duration, Instant};
 
+    //     let task = Interval::new(Instant::now(), Duration::from_millis(100))
+    // //        .take(10)
+    //         .for_each(|instant| {
+    //             println!("fire; instant={:?}", instant);
+    //             Ok(())
+    //         })
+    //         .map_err(|e| panic!("interval errored; err={:?}", e));
 
-//     let task = Interval::new(Instant::now(), Duration::from_millis(100))
-// //        .take(10)
-//         .for_each(|instant| {
-//             println!("fire; instant={:?}", instant);
-//             Ok(())
-//         })
-//         .map_err(|e| panic!("interval errored; err={:?}", e));
+    //     tokio::run(task);
 
-//     tokio::run(task);
-
-    
     Ok(())
 }
-
-
-    
